247e9a8fa8e003eebd4e662e1fe0d7d4cd23fb12
tuxalin <tux_alin@yahoo.com>
2018-01-02 18:08:50 +0200

    disabled as boost is not found

--- bin/ci/script.sh
+++ bin/ci/script.sh
@@ -13,13 +13,13 @@ cd ../../
 source ./bin/ci/common.sh
 cd benchmark
 mkdir build && cd build
-echo "Calling cmake -DBOOST_PREFIX=${BOOST_PREFIX}"
-cmake \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DBGI_ENABLE_CT=ON \
-    -DBOOST_ROOT=${BOOST_PREFIX} \
-    ..
-make -j ${NUMTHREADS}
+#echo "Calling cmake -DBOOST_PREFIX=${BOOST_PREFIX}"
+#cmake \
+#    -DCMAKE_BUILD_TYPE=Release \
+#    -DBGI_ENABLE_CT=ON \
+#    -DBOOST_ROOT=${BOOST_PREFIX} \
+#    ..
+#make -j ${NUMTHREADS}
 
 # Benchmarks run takes long time, skip
 #ctest -C Release -V --output-on-failure .  
