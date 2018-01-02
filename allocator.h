d40cfe3d85d43189a1448766a03a055dafd66d6b
tuxalin <tux_alin@yahoo.com>
2018-01-02 15:49:55 +0200

    cleanup to default allocator

--- allocator.h
+++ allocator.h
@@ -7,7 +7,6 @@
 
 #include <assert.h>
 #include <stdint.h>
-#include <cstddef>
 
 namespace spatial {
 template <class NodeClass> struct heap_allocator {
@@ -16,21 +15,13 @@ template <class NodeClass> struct heap_allocator {
 
   enum { is_overflowable = 0 };
 
-  heap_allocator() : count(0) {}
-
-  ptr_type allocate(int level) {
-    ++count;
-    return new NodeClass(level);
-  }
+  ptr_type allocate(int level) { return new NodeClass(level); }
 
   void deallocate(const ptr_type node) {
-    --count;
     assert(node);
     delete node;
   }
 
   bool overflowed() const { return false; }
-
-  size_t count;
 };
 } // namespace spatial
