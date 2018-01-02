1c6ec2d869b046b111b0764a0c4604ca235063bd
tuxalin <tux_alin@yahoo.com>
2018-01-02 17:19:44 +0200

    travis

--- bin/ci/before_install.sh
+++ bin/ci/before_install.sh
@@ -4,12 +4,8 @@ source ./bin/ci/common.sh
 gcc --version
 clang --version
 cmake --version
-echo "$(tmstamp) *** before_install::svn co boost starting $(date) ***"
-# Boost 1.55+ or trunk is required
-mkdir -p ${BOOST_PREFIX}
-echo "Running svn co ${BOOST_SVN} ${BOOST_HEADERS}"
-svn checkout ${BOOST_SVN} ${BOOST_HEADERS} > /dev/null
-echo "$(tmstamp) *** before_install::svn co boost finished  $(date) ***"
+
+sudo apt-get install libboost-all-dev
 
 echo "Building test examples"
 cd test
