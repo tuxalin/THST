90f6d98bcbb101a60748b5af564df83ee60339cf
tuxalin <tux_alin@yahoo.com>
2018-01-02 18:06:08 +0200

    travis benchmark

--- bin/ci/script.sh
+++ bin/ci/script.sh
@@ -14,11 +14,12 @@ source ./bin/ci/common.sh
 cd benchmark
 mkdir build && cd build
 echo "Calling cmake -DBOOST_PREFIX=${BOOST_PREFIX}"
-#cmake \
-#    -DCMAKE_BUILD_TYPE=Release \
-#    -DBGI_ENABLE_CT=ON \
-#    ..
-#make -j ${NUMTHREADS}
+cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBGI_ENABLE_CT=ON \
+    -DBOOST_ROOT=${BOOST_PREFIX} \
+    ..
+make -j ${NUMTHREADS}
 
 # Benchmarks run takes long time, skip
 #ctest -C Release -V --output-on-failure .  
