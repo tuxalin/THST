bcb4f5270c7978895974218e4a4a53e9684d189f
JosiahWI <josiah_vanderzee@mediacombb.net>
2022-08-15 16:37:56 -0500

    add test for count after iterator insert

--- test/test_quadtree.cpp
+++ test/test_quadtree.cpp
@@ -1,6 +1,9 @@
 #include <doctest/doctest.h>
 #include <QuadTree.h>
 
+#include <array>
+#include <iterator>
+
 template <typename T>
 struct Box2 {
 	T min[2];
@@ -19,15 +22,32 @@ struct Box2 {
 	}
 };
 
-TEST_CASE("quad tree count - value insert") {
+TEST_CASE("test count") {
 	int min[2]{0, 0};
 	int max[2]{1, 1};
 	spatial::BoundingBox<int, 2> bbox{min, max};
 	spatial::QuadTree<int, Box2<int>> qtree{bbox.min, bbox.max};
+	Box2<int> box{{0, 0}, {1, 1}};
 
-	for (int i{0}; i < 3; ++i) {
-		CAPTURE(i);
-		CHECK(qtree.count() == i);
-		qtree.insert(Box2<int>{{0, 0}, {1, 1}});
+	SUBCASE("after value insert") {
+		for (int inserts{0}; inserts < 3; ++inserts) {
+			CAPTURE(inserts);
+			CHECK(qtree.count() == inserts);
+			qtree.insert(Box2<int>{box});
+		}
+	}
+
+	SUBCASE("after iterator insert") {
+		std::array<Box2<int>, 2> boxes{box, box};
+
+		int expected_count{0};
+		for (int inserts{0}; inserts < 2; ++inserts) {
+			CAPTURE(inserts);
+			qtree.insert(
+				std::begin(boxes),
+				std::begin(boxes) + inserts);
+			expected_count += inserts;
+			CHECK(qtree.count() == expected_count);
+		}
 	}
 }
